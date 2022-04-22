59eb624ebb947e957dc2fc1e8c9e5dbdeb3c81bc
fxeqkxplsb <68372244+fxeqkxplsb@users.noreply.github.com>
2022-04-22 22:43:10 +0800

    Create enaeaScriptVideo.js

---
+++ scripts/enaeaScriptVideo.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name         enaeaScriptVideo
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  auto click undone
+// @author       You
+// @match        *://*study.enaea.edu.cn/viewerforccvideo.do*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=enaea.edu.cn
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  var main = function () {
+    var i;
+
+    var replayButton = document.getElementById("replaybtn");
+    if (replayButton) {
+      replayButton.click();
+    }
+
+    // var sourceList = document.getElementsByTagName("source");
+    // if (sourceList.length == 0) setTimeout(location.reload, 8 * 1000);
+
+    var buttonList = document.getElementsByTagName("button");
+
+    if (buttonList.length) {
+      for (i = 0; i < buttonList.length; ++i) {
+        buttonList[i].click();
+      }
+    }
+    var studyProgressList = document.getElementsByClassName(
+      "cvtb-MCK-CsCt-studyProgress"
+    );
+    var titleList = document.getElementsByClassName(
+      "cvtb-MCK-CsCt-title cvtb-text-ellipsis"
+    );
+
+    if (studyProgressList.length) {
+      for (i = 0; i < studyProgressList.length; ++i) {
+        if (studyProgressList[i].innerText !== "100%") {
+          titleList[i].click();
+          break;
+        } else if (i == studyProgressList.length - 1) {
+          window.close();
+        }
+      }
+    }
+
+    return;
+  };
+
+  setInterval(main, 1000);
+})();
